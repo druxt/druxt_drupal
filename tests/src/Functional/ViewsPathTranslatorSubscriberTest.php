a8dcd6fcd28e5a8970cdaf8fc3688213bca6e946
Dave Connerth <captaindav@gmail.com>
2022-05-10 00:42:34 +0000

    feat(#3273228): add test for ViewsPathTranslatorSubscriber

---
+++ tests/src/Functional/ViewsPathTranslatorSubscriberTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\Tests\druxt\Functional;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Url;
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests ViewsPathTranslatorSubscriber for proper handling of
+ * multi-language paths.
+ *
+ * @group druxt
+ */
+class ViewsPathTranslatorSubscriberTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['druxt', 'jsonapi_views']; //??
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Consumer user.
+   *
+   * @var \Drupal\user\Entity\User
+   */
+  protected $consumer;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Create consumer.
+    $this->consumer = $this->createUser(['access druxt resources']);
+    $this->drupalLogin($this->consumer);
+  }
+
+  /**
+   * Tests that Views display paths resolve to the correct view_id / display_id
+   * when a language is specified in the path.
+   */
+  public function testViewsPathTranslatorSubscriber() {
+
+    // Assert that the English version display is returned.
+    $res = $this->drupalGet('/en/recipes');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->responseContains('Deep mediterranean quiche');
+    $output = Json::decode($res);
+    $this->assertNotEmpty($output['data']);
+    $this->assertEquals('recipes', $output['data']['view_id']);
+    $this->assertEquals('page_1', $output['data']['display_id']);
+    // @todo What else to check for in data?
+
+    // Assert that the Spanish version display is returned.
+    $res = $this->drupalGet('/es/recipes');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->responseContains('Quiche mediterráneo profundo');
+    $output = Json::decode($res);
+    $this->assertNotEmpty($output['data']);
+    $this->assertEquals('recipes', $output['data']['view_id']);
+    $this->assertEquals('page_1', $output['data']['display_id']);
+    // @todo What else to check for in data?
+
+  }
+
+}
