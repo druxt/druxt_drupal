576b920f11bcba53c014d6f910529884ef733b5f
Dave Connerth <captaindav@gmail.com>
2022-06-14 16:48:37 -0400

    Issue #3273228 by captaindav: Changes from Deciphered's patch.

--- src/EventSubscriber/ViewsPathTranslatorSubscriber.php
+++ src/EventSubscriber/ViewsPathTranslatorSubscriber.php
@@ -8,11 +8,8 @@ use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Url;
 use Drupal\decoupled_router\EventSubscriber\RouterPathTranslatorSubscriber;
 use Drupal\decoupled_router\PathTranslatorEvent;
-use Drupal\views\Views;
 use Symfony\Cmf\Component\Routing\RouteObjectInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Exception\MethodNotAllowedException;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Event subscriber that processes a path translation with the router info.
@@ -28,18 +25,12 @@ class ViewsPathTranslatorSubscriber extends RouterPathTranslatorSubscriber {
       $this->logger->error('Unable to get the response object for the decoupled router event.');
       return;
     }
-
-    // @todo jsonapi_views is dependency in druxt.info.yml, it is always enabled, can't we eleminate this check?
     if (!$this->moduleHandler->moduleExists('jsonapi_views')) {
       return;
     }
 
     $path = $event->getPath();
     $path = $this->cleanSubdirInPath($path, $event->getRequest());
-    if ($this->languageManager->isMultilingual()) {
-      $path = $this->getPathFromAlias($path);
-    }
-
     try {
       $match_info = $this->router->match($path);
     }
@@ -52,30 +43,24 @@ class ViewsPathTranslatorSubscriber extends RouterPathTranslatorSubscriber {
           'resolved' => $path,
         ]);
       }
-      // @todo shouldn't there be an else { $response->setStatusCode(404)?
       return;
     }
     catch (MethodNotAllowedException $exception) {
-      // @todo Shouldn't this be a 405 not a 403?
+      // @todo Shouldn't this be a 405 not 403
       $response->setStatusCode(403);
+      return;
     }
 
     $entity_type_manager = $this->container->get('entity_type.manager');
     $views_storage = $entity_type_manager->getStorage('view');
     $view = $views_storage->load($match_info['view_id']);
-    $executable = Views::executableFactory()->get($view);
-    $executable->setDisplay($match_info['display_id']);
 
-    // Determine langcode.
-    $langcode = NULL;
-    $language = NULL;
     if ($this->languageManager->isMultilingual()) {
       $destination = parse_url($event->getPath(), PHP_URL_PATH);
       $language_negotiation_url = $this->languageManager->getNegotiator()
         ->getNegotiationMethodInstance('language-url');
       $router_request = Request::create($destination);
       $langcode = $language_negotiation_url->getLangcode($router_request);
-      $language = $this->languageManager->getLanguage($langcode);
     }
 
     $route = $match_info[RouteObjectInterface::ROUTE_OBJECT];
@@ -84,7 +69,7 @@ class ViewsPathTranslatorSubscriber extends RouterPathTranslatorSubscriber {
       [],
       [
         'absolute' => TRUE,
-        'language' => $language
+        'language' => $langcode
       ]
     )->toString(TRUE);
     $response->addCacheableDependency($resolved_url);
@@ -101,9 +86,8 @@ class ViewsPathTranslatorSubscriber extends RouterPathTranslatorSubscriber {
         'uuid' => $view->get('uuid'),
         'view_id' => $match_info['view_id'],
         'display_id' => $match_info['display_id'],
-        'langcode' => $langcode
       ],
-      'label' => $executable->getTitle(),
+      'label' => $match_info['_title'],
     ];
 
     // If the route is JSON API, it means that JSON API is installed and its
@@ -116,24 +100,14 @@ class ViewsPathTranslatorSubscriber extends RouterPathTranslatorSubscriber {
       $rt = $rt_repo->get($view_type_id, $view->bundle());
       $type_name = $rt->getTypeName();
       $jsonapi_base_path = $this->container->getParameter('jsonapi.base_path');
-      $entry_point_url = Url::fromRoute(
-        'jsonapi.resource_list',
-        [],
-        [
-          'absolute' => TRUE,
-          'language' => $language,
-        ]
-      )->toString(TRUE);
+      $entry_point_url = Url::fromRoute('jsonapi.resource_list', [], ['absolute' => TRUE])->toString(TRUE);
       $route_name = sprintf('jsonapi.%s.individual', $type_name);
       $individual = Url::fromRoute(
         $route_name,
         [
           static::getEntityRouteParameterName($route_name, $view_type_id) => $view->uuid(),
         ],
-        [
-          'absolute' => TRUE,
-          'language' => $language,
-        ]
+        ['absolute' => TRUE]
       )->toString(TRUE);
       $response->addCacheableDependency($entry_point_url);
       $response->addCacheableDependency($individual);
@@ -161,14 +135,7 @@ class ViewsPathTranslatorSubscriber extends RouterPathTranslatorSubscriber {
         $match_info['display_id'],
       ];
       $jsonapi_views_route = implode('.', $parts);
-      $resolved_jsonapi_views_url = Url::fromRoute(
-        $jsonapi_views_route,
-        [],
-        [
-          'absolute' => TRUE,
-          'language' => $language
-        ]
-      )->toString(TRUE);
+      $resolved_jsonapi_views_url = Url::fromRoute($jsonapi_views_route, [], ['absolute' => TRUE])->toString(TRUE);
       $response->addCacheableDependency($resolved_jsonapi_views_url);
 
       $output['jsonapi_views'] = $resolved_jsonapi_views_url->getGeneratedUrl();
