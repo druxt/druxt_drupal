3465ebdff5dbe688593af940b4a967744e891e5d
Stuart Clark <10007-deciphered@users.noreply.drupalcode.org>
2025-08-05 02:04:18 +0000

    Issue #3315030 by realityloop, deciphered: Add a wildcard route translator service to catch all available routes

---
+++ src/EventSubscriber/WildcardPathTranslatorSubscriber.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\druxt\EventSubscriber;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Cache\CacheableJsonResponse;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Routing\RouteObjectInterface;
+use Drupal\Core\Url;
+use Drupal\decoupled_router\EventSubscriber\RouterPathTranslatorSubscriber;
+use Drupal\decoupled_router\PathTranslatorEvent;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+/**
+ * Event subscriber that processes a path translation with the router info.
+ */
+class WildcardPathTranslatorSubscriber extends RouterPathTranslatorSubscriber {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onPathTranslation(PathTranslatorEvent $event) {
+    $response = $event->getResponse();
+    if (!$response instanceof CacheableJsonResponse) {
+      $this->logger->error('Unable to get the response object for the decoupled router event.');
+      return;
+    }
+
+    $path = $event->getPath();
+    $path = $this->cleanSubdirInPath($path, $event->getRequest());
+    try {
+      $match_info = $this->router->match($path);
+    }
+    catch (ResourceNotFoundException $exception) {
+      // If URL is external, we won't perform checks for content in Drupal,
+      // but assume that it's working.
+      if (UrlHelper::isExternal($path)) {
+        $response->setStatusCode(200);
+        $response->setData([
+          'resolved' => $path,
+        ]);
+      }
+      return;
+    }
+    catch (MethodNotAllowedException $exception) {
+      $response->setStatusCode(403);
+      return;
+    }
+
+    $route = $match_info[RouteObjectInterface::ROUTE_OBJECT];
+    try {
+      $resolved_url = Url::fromRoute($route, [], ['absolute' => TRUE])->toString(TRUE);
+    }
+    catch (MissingMandatoryParametersException $exception) {
+      $response->setData([
+        'message' => 'Unable to build route URL.',
+        'details' => $exception->getMessage(),
+      ]);
+      $response->setStatusCode(500);
+      return;
+    }
+    $response->addCacheableDependency($resolved_url);
+
+    $is_home_path = $this->resolvedPathIsHomePath($resolved_url->getGeneratedUrl());
+    $response->addCacheableDependency(
+      (new CacheableMetadata())->setCacheContexts(['url.path.is_front'])
+    );
+
+    $output = [
+      'resolved' => $resolved_url->getGeneratedUrl(),
+      'isHomePath' => $is_home_path,
+      'label' => $match_info['_title'],
+      'context' => $match_info
+    ];
+
+    $response->setStatusCode(200);
+    $response->setData($output);
+
+    $event->stopPropagation();
+  }
+
+}
