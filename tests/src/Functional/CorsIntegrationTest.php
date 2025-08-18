e94b580ace6101e7b3880a0b1f041c81b1bc86e5
Stuart Clark <10007-deciphered@users.noreply.drupalcode.org>
2025-08-18 00:11:32 +0000

    feat(#3541756): set default CORS allowedHeaders.

--- tests/src/Functional/CorsIntegrationTest.php
+++ tests/src/Functional/CorsIntegrationTest.php
@@ -27,6 +27,7 @@ class CorsIntegrationTest extends BrowserTestBase {
   public function testCrossSiteRequestEnabled() {
     $cors_config = $this->container->getParameter('cors.config');
     $this->assertTrue($cors_config['enabled']);
+    $this->assertContains('*', $cors_config['allowedHeaders']);
   }
 
 }
